6b40a8650d8a5ec578d577d803d683493973fc92
Expensiveee <TheExpensiveee@gmail.com>
2023-07-23 19:51:21 +0100

    Update Text component

--- apps/web/components/ui/Text.tsx
+++ apps/web/components/ui/Text.tsx
@@ -3,21 +3,32 @@ import { cva, type VariantProps } from "class-variance-authority";
 const text = cva("", {
   variants: {
     size: {
-      display: ["text-6xl", "font-black", "capitalize"],
-      h1: ["text-4xl", "font-extrabold"],
-      h2: ["text-3xl", "font-bold"],
-      h3: ["text-2xl", "font-bold"],
-      h4: ["text-xl", "font-semibold"],
-      h5: ["text-lg", "font-semibold"],
-      h6: ["text-base", "font-medium"],
-      body1: ["text-md", "font-normal"],
-      body2: ["text-sm", "font-normal"],
-      caption: ["text-xs", "font-light"],
+      display: ["text-7xl"],
+      h1: ["text-6xl"],
+      h2: ["text-5xl"],
+      h3: ["text-4xl"],
+      h4: ["text-3xl"],
+      h5: ["text-2xl"],
+      h6: ["text-xl"],
+      body1: ["text-base"],
+      body2: ["text-md"],
+      caption: ["text-sm"],
     },
     color: {
       white: ["text-white"],
       primary: ["text-primary-500"],
-      gray: ["text-dark-400"],
+      gray: ["text-neutral-200"],
+    },
+    weight: {
+      black: ["font-black"],
+      extrabold: ["font-extrabold"],
+      bold: ["font-bold"],
+      semibold: ["font-semibold"],
+      medium: ["font-medium"],
+      normal: ["font-normal"],
+      light: ["font-light"],
+      extralight: ["font-extralight"],
+      thin: ["font-thin"],
     },
   },
   defaultVariants: {
@@ -27,11 +38,11 @@ const text = cva("", {
 
 //Exluding color from the props because we want to use our own color prop
 export interface TextProps extends Omit<React.HTMLAttributes<HTMLElement>, "color">, VariantProps<typeof text> {
-  tag?: React.ElementType;
+  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "p";
 }
 
-export default function Text({ className, tag = "span", size, color, ...props }: TextProps) {
+export default function Text({ className, tag = "span", size, color, weight, ...props }: TextProps) {
   const Tag = tag;
 
-  return <Tag {...props} className={text({ size, color, className })} />;
+  return <Tag {...props} className={text({ size, color, className, weight })} />;
 }
