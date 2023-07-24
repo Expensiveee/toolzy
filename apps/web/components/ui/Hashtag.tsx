f27d1cfad5778a6e5c9f99a7bcd10a96e6204154
Expensiveee <TheExpensiveee@gmail.com>
2023-07-24 14:09:50 +0100

    Update Hashtag to optional emoji

--- apps/web/components/ui/Hashtag.tsx
+++ apps/web/components/ui/Hashtag.tsx
@@ -3,11 +3,11 @@ import { cva, cx, type VariantProps } from "class-variance-authority";
 import Image from "next/image";
 
 const hashtag = cva(
-  "rounded-full bg-dark-700 hover:bg-dark-700/70 border border-dark-400/40 text-white cursor-pointer px-4 py-2.5 text-sm transition-colors duration-300 ease-in-out font-normal"
+  "rounded-full w-fit bg-dark-700 hover:bg-dark-700/70 border border-dark-400/40 text-white cursor-pointer px-4 py-2.5 text-sm transition-colors duration-300 ease-in-out font-normal"
 );
 
 export interface HashtagProps extends React.HTMLAttributes<HTMLSpanElement>, VariantProps<typeof hashtag> {
-  emoji: string;
+  emoji?: string;
   text: string;
 }
 
@@ -17,7 +17,7 @@ const Hashtag: React.FC<HashtagProps> = ({ className, text, emoji, ...props }) =
   return (
     <span {...props} className={combinedClassName}>
       #{text}
-      <Image alt={`${emoji} emoji`} src={`/emojis/${emoji}.svg`} height={16} width={16} />
+      {emoji && <Image alt={`${emoji} emoji`} src={`/emojis/${emoji}.svg`} height={16} width={16} />}
     </span>
   );
 };
