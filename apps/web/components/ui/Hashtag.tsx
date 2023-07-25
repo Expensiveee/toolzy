5f7cc08ecc11eea18694c35efaecb7ec669d4c44
Expensiveee <TheExpensiveee@gmail.com>
2023-07-25 21:39:07 +0100

    Add themes to Hashtag

--- apps/web/components/ui/Hashtag.tsx
+++ apps/web/components/ui/Hashtag.tsx
@@ -3,7 +3,15 @@ import { cva, cx, type VariantProps } from "class-variance-authority";
 import Image from "next/image";
 
 const hashtag = cva(
-  "rounded-full w-fit bg-dark-700 hover:bg-dark-700/70 border border-dark-400/40 text-white cursor-pointer px-4 py-2.5 text-sm transition-colors duration-300 ease-in-out font-normal"
+  "rounded-full w-fit cursor-pointer px-4 py-2.5 border text-sm transition-colors duration-300 ease-in-out font-medium",
+  {
+    variants: {
+      theme: {
+        dark: ["bg-dark-700", "hover:bg-dark-600", "text-white", "border-dark-400/40"],
+        light: ["bg-white", "hover:bg-gray-100", "text-zinc-800", "border-zinc-400/40"],
+      },
+    },
+  }
 );
 
 export interface HashtagProps extends React.HTMLAttributes<HTMLSpanElement>, VariantProps<typeof hashtag> {
@@ -11,8 +19,8 @@ export interface HashtagProps extends React.HTMLAttributes<HTMLSpanElement>, Var
   text: string;
 }
 
-const Hashtag: React.FC<HashtagProps> = ({ className, text, emoji, ...props }) => {
-  const combinedClassName = cx(hashtag({ className }), "flex select-none justify-center items-center gap-1");
+const Hashtag: React.FC<HashtagProps> = ({ className, text, emoji, theme, ...props }) => {
+  const combinedClassName = cx(hashtag({ theme, className }), "flex select-none justify-center items-center gap-1");
 
   return (
     <span {...props} className={combinedClassName}>
