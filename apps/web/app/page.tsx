cee385bd251217018cfe220e0e0b88bd05837741
Expensiveee <TheExpensiveee@gmail.com>
2023-07-27 20:40:19 +0100

    Add FAQ section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,18 @@
+import { Crop, Search, Users, RefreshCcw, FileText, Droplet, ArrowRight } from "react-feather";
 import Container from "@web/components/Container";
 import Text from "@web/components/ui/Text";
 import DynamicText from "@web/components/DynamicText";
 import Input from "@web/components/ui/Input";
 import Button from "@web/components/ui/Button";
-import { Crop, Search, Users, RefreshCcw, Droplet, Camera, Image, ArrowRight } from "react-feather";
 import Hashtag from "@web/components/ui/Hashtag";
 import Section from "@web/components/Section";
 import ToolCard from "@web/components/ui/ToolCard";
+import Accordion from "@web/components/ui/Accordion";
 
 const words = ["developers", "designers", "Marketers", "Innovators", "You"];
-const features = [
+const statistics = [
   {
-    title: (
+    name: (
       <>
         Over 10+ <span className="text-primary-500">free</span> online tools
       </>
@@ -25,7 +26,7 @@ const features = [
     icon: <Crop className="text-primary-500 shrink-0 grow-0" size={32} />,
   },
   {
-    title: (
+    name: (
       <>
         1,000+ <span className="text-primary-500">active</span> users
       </>
@@ -38,7 +39,7 @@ const features = [
     icon: <Users className="text-primary-500 shrink-0 grow-0" size={32} />,
   },
   {
-    title: (
+    name: (
       <>
         New tools <span className="text-primary-500">regularly</span>
       </>
@@ -51,47 +52,47 @@ const features = [
     icon: <RefreshCcw className="text-primary-500 shrink-0 grow-0" size={32} />,
   },
 ];
-const tools = [
+const features = [
   {
-    title: "Image Color Picker",
-    square: <div className={`h-10 w-10 shrink-0 rounded-md bg-emerald-500`}></div>,
-    icon: <Droplet className="text-gray-400" size={22} />,
+    name: "Myriad Of Tools",
+    description:
+      "Step into a realm of boundless possibilities tailored just for you. Discover a dazzling collection of delightful tools that promise to elevate your daily experiences. Embrace the joy of effortless productivity as you immerse yourself in the brilliance of our toolkit",
   },
   {
-    title: "SVG to PNG Converter",
-    square: <div className={`h-10 w-10 shrink-0 rounded-md bg-violet-500`}></div>,
-    icon: <Image className="text-gray-400" size={22} />,
+    name: "The Art of Efficiency",
+    description:
+      "One of the standout features of Toolzy is its user-friendly interface and seamless user experience. The platform is designed with simplicity in mind, making it easy for users to navigate and use the tools effortlessly",
   },
   {
-    title: "Metadata tag viewer",
-    square: <div className={`h-10 w-10 shrink-0 rounded-md bg-red-500`}></div>,
-    icon: <Camera className="text-gray-400" size={22} />,
+    name: "Tool-tastic Innovations",
+    description:
+      "Innovation is our heart! Discover fresh, cutting-edge tools that keep pace with your evolving needs. Stay ahead of the curve and unleash your full potential with trendy features",
   },
 ];
 const topPicks = [
   {
-    title: "Image Color Picker",
+    name: "Image Color Picker",
     description:
       "Pick the perfect color from any image with ease. Extract colors for your designs, artworks, or projects effortlessly. Upload an image, click on any spot, and get the precise color code you need. Create stunning visuals with this efficient tool.",
     href: "/tools/image-color-picker",
     rating: 4.5,
   },
   {
-    title: "SVG to PNG Converter",
+    name: "SVG to PNG Converter",
     description:
       "Convert SVG files to PNG format seamlessly. Transform scalable vector graphics into pixel-perfect PNG images. Perfect for web design or social media projects. Upload SVG, select the size, and let the converter work. Save time with this user-friendly tool.",
     href: "/tools/svg-to-png-converter",
     rating: 4.5,
   },
   {
-    title: "Metadata tag viewer",
+    name: "Metadata tag viewer",
     description:
       "Explore hidden metadata tags of any website. Get insights into site structure, keywords, and more. Ideal for SEO specialists or curious web enthusiasts. Enter the URL, and the viewer displays relevant tags. Uncover website magic with this insightful tool.",
     href: "/tools/metatag-data-viewer",
     rating: 4.3,
   },
   {
-    title: "PDF Merger",
+    name: "PDF Merger",
     description:
       "Merge multiple PDF files effortlessly. Say goodbye to scattered files. Combine reports, presentations, or documents. Upload PDFs, arrange order, and create a unified file with a click. Enhance productivity with this efficient PDF Merger.",
     href: "/tools/pdf-merger",
@@ -103,28 +104,30 @@ export default async function Index() {
   return (
     <Container className="flex flex-col items-center justify-center">
       <Section className="relative flex flex-col items-center bg-[url('/assets/hero-blur.svg')] bg-cover bg-bottom text-center">
-        <Text className="mb-8 capitalize" tag="h1" weight={"black"} size="display">
-          Hub of <span className="text-primary-500">free online tools</span>{" "}
-          <span className="inline lg:block">
-            made for <DynamicText className="font-light" words={words} />
-          </span>
-        </Text>
+        <header className="w-full">
+          <Text className="mb-8 capitalize" tag="h1" weight={"black"} size="display">
+            Hub of <span className="text-primary-500">free online tools</span>{" "}
+            <span className="inline lg:block">
+              made for <DynamicText className="font-light" words={words} />
+            </span>
+          </Text>
 
-        <Text className="mb-20" tag="h2" size={"h6"} weight={"light"} color="lightgray">
-          We are a community of developers, designers, and product managers{" "}
-          <span className="ineline lg:block">who are passionate about building products that people love.</span>
-        </Text>
+          <Text className="mb-20" tag="h2" size={"h6"} weight={"light"} color="lightgray">
+            We are a community of developers, designers, and product managers{" "}
+            <span className="ineline lg:block">who are passionate about building products that people love.</span>
+          </Text>
 
-        <div className="mb-12 flex w-full items-center justify-center gap-2 px-4 lg:px-0">
-          <div className="bg-dark-700 border-dark-400/40 flex w-full items-center rounded-xl border p-1 lg:w-2/5">
-            <Search size={22} className="m-2 hidden shrink-0 grow-0 text-neutral-500 lg:inline" />
-            <Input className="ml-3 py-2 lg:ml-0 lg:mr-2 lg:py-2" placeholder="Find the perfect tool" theme="none" />
-            <Button className="lg:w-22 hidden lg:inline" size="large">
-              Search
-            </Button>
-            <Button variant="icon" icon={<Search size={18} />} className="inline lg:hidden" size="medium" />
+          <div className="mb-12 flex w-full items-center justify-center gap-2 px-4 lg:px-0">
+            <div className="bg-dark-700 border-dark-400/40 flex w-full items-center rounded-xl border p-1 lg:w-2/5">
+              <Search size={22} className="m-2 hidden shrink-0 grow-0 text-neutral-500 lg:inline" />
+              <Input className="ml-3 py-2 lg:ml-0 lg:mr-2 lg:py-2" placeholder="Find the perfect tool" theme="none" />
+              <Button className="lg:w-22 hidden lg:inline" size="large">
+                Search
+              </Button>
+              <Button variant="icon" icon={<Search size={18} />} className="inline lg:hidden" size="medium" />
+            </div>
           </div>
-        </div>
+        </header>
 
         <div className="mb-12 flex gap-6">
           <Hashtag theme="dark" text="Design" emoji={"palette"} />
@@ -133,19 +136,19 @@ export default async function Index() {
         </div>
 
         <div className="flex w-full select-none flex-col items-stretch justify-center gap-6 lg:flex-row">
-          {features.map((feature, i) => (
+          {statistics.map((s, i) => (
             <div
               key={i}
               className="bg-dark-700 border-dark-400/40 flex h-auto w-full flex-col items-start justify-start rounded-xl border px-8 py-8 text-left"
             >
-              {feature.icon}
+              {s.icon}
 
               <Text className="mt-4" tag="h3" size="h5" weight="bold">
-                {feature.title}
+                {s.name}
               </Text>
 
-              <Text className="mt-2 capitalize" color="lightgray" tag="p" size="body1">
-                {feature.description}
+              <Text className="mt-2 capitalize" color="lightgray" tag="p" size="h6">
+                {s.description}
               </Text>
             </div>
           ))}
@@ -153,22 +156,26 @@ export default async function Index() {
       </Section>
       <Section className="bg-primary-500 flex-col-reverse items-center gap-20 lg:flex-row lg:gap-32">
         <div className="flex w-full flex-1 flex-col items-center lg:py-10">
-          <div className="flex w-full flex-col gap-6 rounded-2xl lg:aspect-square lg:w-max lg:bg-gray-100">
-            {tools.map((tool, i) => (
+          <div className="flex w-full flex-col items-stretch gap-4 rounded-2xl lg:w-max lg:bg-slate-50">
+            {features.map((f, i) => (
               <div
                 key={i}
-                className=" border-dark-200 flex w-full cursor-pointer flex-col items-center justify-center space-y-5 rounded-xl border bg-white px-4 py-8 shadow-xl transition-transform duration-300 ease-in-out hover:scale-105 lg:translate-x-12 lg:translate-y-12"
+                className="border-dark-200 flex w-full cursor-pointer flex-col items-center justify-center space-y-5 rounded-xl border bg-white px-4 py-8 shadow-xl lg:translate-x-12 lg:translate-y-12"
               >
                 <div className="flex w-full items-start justify-start gap-8">
-                  {tool.square}
-                  <div className="flex flex-1 flex-col gap-3">
-                    <Text tag="h4" size="h6" weight={"extrabold"} color="black">
-                      {tool.title}
-                    </Text>
-                    <div className="mt-2 h-2.5 w-full rounded-xl bg-gray-300"></div>
-                    <div className="h-2.5  w-4/5 rounded-xl bg-gray-300"></div>
+                  <div className={`bg-primary-500 flex h-10 w-10 shrink-0 items-center justify-center rounded-md`}>
+                    {i + 1}
                   </div>
-                  {tool.icon}
+                  <div className="flex flex-1 flex-col gap-3">
+                    <Text tag="h4" size="h6" weight="extrabold" color="black">
+                      {f.name}
+                    </Text>
+                    <div className="mt-2 w-full lg:max-w-[350px] 2xl:max-w-[550px]">
+                      <Text className="pr-4" color="darkgray" weight="medium" tag="p" size="body1">
+                        {f.description}
+                      </Text>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
@@ -176,12 +183,12 @@ export default async function Index() {
         </div>
 
         <div className="flex w-full flex-1 flex-col items-center">
-          <Hashtag className="mb-2 lg:self-start" theme="light" text="SimplifyYourWorkflow" emoji={"palette"} />
+          <Hashtag className="mb-2 lg:self-start" theme="light" text="Features" emoji={"palette"} />
           <Text tag="h3" className="mb-4 mt-6 w-full text-left capitalize" weight={"extrabold"} size={"h3"}>
             Where Magic Meets Efficiency
           </Text>
-          <Text tag="p" className="mb-10 capitalize" size="body1">
-            Toolzy, your One-Stop Hub for Online Tools. We&apos;ve got the essentials tailored for designers,
+          <Text tag="p" className="mb-10" size="h6">
+            Toolzy, your One-Stop hub for online tools. We&apos;ve got the essentials tailored for designers,
             developers, marketers and much more. Simplify your workflow and achieve remarkable results by elevating your
             skills and your creativity.
           </Text>
@@ -208,30 +215,120 @@ export default async function Index() {
           >
             Top Picks: The Fabulous Four
           </Text>
-          <Text tag="p" className="mb-10 capitalize" size="body1">
-            Introducing <span className="font-bold">Top Picks</span> - A Carefully Curated Collection of Our Most
-            Popular and Essential Online Tools. These Handpicked Gems Are Loved for Their Remarkable Performance,
-            Versatility, and Positive User Reviews. Whether You&apos;re a Design Guru, Coding Wizard, or Marketing
-            Maverick, These Tools Are Your Secret Weapons to Boost Productivity and Unleash Your Creative Brilliance
+          <Text tag="p" className="mb-10" size="h5">
+            Introducing <span className="font-black">Top Picks</span> - A carefully curated collection of our most
+            popular and essential online tools. These handpicked gems are loved for their remarkable performance,
+            versatility, and positive user reviews. Whether you&apos;re a design guru, coding wizard, or marketing
+            maverick, these tools are your secret weapons to boost productivity and unleash your creative brilliance
           </Text>
         </div>
 
-        <div className="flex flex-col  items-center justify-center w-full 2xl:w-3/5">
+        <div className="flex w-full  flex-col items-center justify-center 2xl:w-3/5">
           <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
             {topPicks.map((pick, i) => (
               <ToolCard
                 theme={i % 3 ? "dark" : "primary"}
                 key={i}
-                title={pick.title}
+                title={pick.name}
                 description={pick.description}
                 href={pick.href}
                 rating={pick.rating}
               />
             ))}
           </div>
-          <Text className="mt-10" size="body1" color="lightgray">
-            And much more
+        </div>
+      </Section>
+      <Section className="bg-primary-500 flex flex-col items-center gap-20 xl:flex-row">
+        <div className="w-3/5">
+          <Hashtag className="mb-2 self-center lg:self-start" theme="light" text="FrequentlyAskedQuestions" />
+
+          <Text
+            tag="h3"
+            className="mb-4 mt-6 flex w-full items-center text-left capitalize"
+            weight={"extrabold"}
+            size={"h3"}
+          >
+            FAQ - Answering Your Curiosities
           </Text>
+          <Text tag="p" className="mb-10" size="h5">
+            Find quick answers to your burning questions and discover the magic of your new favorite website. If youare
+            more curious than or have any other inquires, feel free to contact us using the form below.
+          </Text>
+
+          {/* TODO: FAQ SERP */}
+
+          <div className="flex flex-col gap-4 2xl:w-4/5">
+            <Accordion
+              title="Are all the tools on Toolzy really free to use?"
+              description="Absolutely! All the tools available on Toolzy are completely free to use. There are no hidden charges or subscription fees."
+            />
+
+            <Accordion
+              title="How often are new tools added to Toolzy?"
+              description="We are constantly innovating and adding new tools to our platform. We strive to provide you with fresh and cutting-edge tools regularly, so you can stay updated with the latest trends and technologies."
+            />
+
+            <Accordion
+              title="Is my data safe on Toolzy?"
+              description="Absolutely! We take data privacy and security very seriously. Your information is protected with industry-standard encryption and security measures."
+            />
+
+            <Accordion
+              title="Can I suggest a new tool idea to be added to Toolzy?"
+              description="Of course! We welcome your suggestions and feedback. If you have an idea for a new tool or feature, feel free to reach out to us through our contact form. We love hearing from our users!"
+            />
+
+            <Accordion
+              title="How can I contact customer support if I have a question or issue?"
+              description="If you have any questions or encounter any issues while using Toolzy, you can reach out to our dedicated customer support team through the 'Contact Us' page. We are here to assist you and ensure you have a smooth experience on our platform."
+            />
+          </div>
+        </div>
+        <div className="flex w-2/5 flex-col justify-center">
+          <div className="flex w-full flex-col gap-6 rounded-2xl lg:aspect-square lg:w-max lg:bg-gray-100">
+            <div className=" border-dark-200 flex w-full cursor-pointer flex-col items-center justify-between gap-y-10 space-y-5 rounded-xl border bg-white px-6 py-8 shadow-2xl transition-transform duration-300 ease-in-out hover:scale-105 lg:translate-x-12 lg:translate-y-12">
+              <div className="flex w-full items-start justify-start gap-8">
+                <div className={`h-10 w-10 shrink-0 rounded-md bg-pink-500 shadow-lg`}></div>
+                <div className="flex flex-1 flex-col gap-3">
+                  <Text tag="h4" size="h6" weight={"extrabold"} color="black">
+                    Image Color Picker
+                  </Text>
+                  <div className="mt-2 h-2.5 w-full rounded-xl bg-gray-300"></div>
+                  <div className="h-2.5  w-4/5 rounded-xl bg-gray-300"></div>
+                </div>
+                <Droplet className="text-gray-400" size={22} />
+              </div>
+              <div className="flex w-full gap-12">
+                <div className="bg-primary-500 h-32 w-32 shrink-0 rounded-md shadow-xl"></div>
+                <div className="flex flex-col">
+                  <Text tag="p" size="body1" weight={"extrabold"} color="darkgray">
+                    Color Selected
+                  </Text>
+                  <Text className="mt-1" tag="p" size="body2" weight={"semibold"} color="darkgray">
+                    #ff2148
+                  </Text>
+                  <div className="mt-3 flex gap-1">
+                    <div className={`h-6 w-6 shrink-0 rounded-md bg-green-500`}></div>
+                    <div className={`h-6 w-6 shrink-0 rounded-md bg-cyan-500`}></div>
+                    <div className={`h-6 w-6 shrink-0 rounded-md bg-orange-500`}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="border-dark-200 flex w-full cursor-pointer flex-col items-center justify-center space-y-5 rounded-xl border bg-white px-6 py-8 shadow-2xl transition-transform duration-300 ease-in-out hover:scale-105 lg:translate-x-12 lg:translate-y-12">
+              <div className="flex w-full items-start justify-start gap-8">
+                <div className="h-10 w-10 shrink-0 rounded-md bg-emerald-500 shadow-lg"></div>
+                <div className="flex flex-1 flex-col gap-3">
+                  <Text tag="h4" size="h6" weight={"extrabold"} color="black">
+                    PDF Merger
+                  </Text>
+                  <div className="mt-2 h-2.5 w-full rounded-xl bg-gray-300"></div>
+                  <div className="h-2.5  w-4/5 rounded-xl bg-gray-300"></div>
+                </div>
+                <FileText className="text-gray-400" size={22} />
+              </div>
+            </div>
+          </div>
         </div>
       </Section>
     </Container>
