724e545b5f62c9c31d8f79de61a1c2cc798d4da3
Expensiveee <TheExpensiveee@gmail.com>
2023-07-27 16:43:51 +0100

    Remove support link and add structured data features

--- apps/web/components/navigation/Navbar.tsx
+++ apps/web/components/navigation/Navbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import React from "react";
 import Link from "@web/components/ui/Link";
 import Button from "@web/components/ui/Button";
-import { FileText, Heart, Home, LifeBuoy, Tool } from "react-feather";
+import { FileText, Heart, Home, Tool } from "react-feather";
 import { usePathname } from "next/navigation";
 
 const navItems = [
@@ -26,37 +27,42 @@ const navItems = [
     href: "/contribute",
     icon: Heart,
   },
-  {
-    name: "Support",
-    href: "/support",
-    icon: LifeBuoy,
-  },
 ];
-
-// TODO: Make the navbar responsive
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-dark-700 sticky left-0 top-0 z-50 mx-auto flex h-16 w-full max-w-[1800px] items-center justify-between px-8">
+    <nav
+      className="bg-dark-700 z-50 mx-auto flex h-16 w-full max-w-[1800px] items-center justify-between px-8"
+      itemScope
+      itemType="https://schema.org/SiteNavigationElement"
+    >
       <div className="flex items-center">
-        <p className="mr-8 text-3xl font-bold text-white">Toolzy</p>
-        <ul className="flex gap-2">
-          {navItems.map((item, i) => (
-            <li key={i}>
-              <Link
-                className={`${pathname === item.href ? "text-primary-500" : ""}`}
-                variant="text-icon"
-                icon={<item.icon size={16} />}
-                href={item.href}
-              >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <Link href="/" itemProp="url">
+          <p className="text-3xl font-bold text-white" itemProp="name">
+            Toolzy
+          </p>
+        </Link>
       </div>
+      <ul className="flex gap-2">
+        {navItems.map((item, i) => (
+          <li key={i} itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
+            <Link
+              className={`${pathname === item.href ? "text-primary-500" : ""}`}
+              variant="text-icon"
+              icon={<item.icon size={16} />}
+              href={item.href}
+              itemProp="item"
+              itemScope
+              itemType="https://schema.org/Thing"
+            >
+              <span itemProp="name">{item.name}</span>
+              <meta itemProp="position" content={`${i + 1}`} />
+            </Link>
+          </li>
+        ))}
+      </ul>
       <div className="flex items-center justify-end gap-2">
         <Button theme="primary" size={"medium"}>
           Login
