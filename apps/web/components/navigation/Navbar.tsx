496e97596105185af8aabcbb9eb2c153f33291fd
Expensiveee <TheExpensiveee@gmail.com>
2023-07-27 16:51:20 +0100

    Added schema.org markup for SiteNavigationElement in Navbar

--- apps/web/components/navigation/Navbar.tsx
+++ apps/web/components/navigation/Navbar.tsx
@@ -33,32 +33,21 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav
-      className="bg-dark-700 z-50 mx-auto flex h-16 w-full max-w-[1800px] items-center justify-between px-8"
-      itemScope
-      itemType="https://schema.org/SiteNavigationElement"
-    >
+    <nav className="bg-dark-700 z-50 mx-auto flex h-16 w-full max-w-[1800px] items-center justify-between px-8">
       <div className="flex items-center">
-        <Link href="/" itemProp="url">
-          <p className="text-3xl font-bold text-white" itemProp="name">
-            Toolzy
-          </p>
-        </Link>
+        <p className="text-3xl font-bold text-white">Toolzy</p>
       </div>
       <ul className="flex gap-2">
         {navItems.map((item, i) => (
-          <li key={i} itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
+          <li key={i} itemScope itemType="https://schema.org/SiteNavigationElement">
             <Link
               className={`${pathname === item.href ? "text-primary-500" : ""}`}
               variant="text-icon"
               icon={<item.icon size={16} />}
               href={item.href}
-              itemProp="item"
-              itemScope
-              itemType="https://schema.org/Thing"
+              itemProp="url"
             >
               <span itemProp="name">{item.name}</span>
-              <meta itemProp="position" content={`${i + 1}`} />
             </Link>
           </li>
         ))}
